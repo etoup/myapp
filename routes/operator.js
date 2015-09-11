b7acda97c6b1c8c3c46fcf9f5796b69b8c30761d
etoup <etoupcom@126.com>
2015-09-11 15:52:43 +0800

    add files

--- routes/operator.js
+++ routes/operator.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('operator/index', { title: 'Express' });
+  res.render('operator/index', { title: '运营方后台' });
 });
 
 module.exports = router;
